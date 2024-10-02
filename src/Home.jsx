381a2e966c14104103dda62a2d40db4ca22a76e8
Alec Kienzle <akienzle@opentext.com>
2024-10-02 14:43:58 -0700

    getSampleFiles fetch request working

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,26 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useAuth } from 'react-oidc-context';
 import { Box, Button } from "@mui/material";
 import Grid from '@mui/material/Grid2';
 
 export const Home = () => {
   const { user } = useAuth();
+  const [ sampleFiles, setSampleFiles ] = useState();
 
-  const getFolders = async () => {
-    const response = await fetch('proxy/cms/instances/folder/cms_folder', {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${user.access_token}`,
-      }
-    });
-    const responseJson = await response.json();
+  const getSampleFiles = async() => {
+    const requestOptions = { method: 'GET', headers: { 'Authorization': `Bearer ${user.access_token}` } }
+    const response = await fetch(`proxy/cms/instances/folder/cms_folder/${import.meta.env.VITE_SAMPLES_FOLDER_ID}`, requestOptions);
+    const responseJson = await response.json()
     console.log(responseJson);
+    setSampleFiles(responseJson);  
   }
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2} minHeight={160}>
         <Grid display="flex" justifyContent="center" alignItems="center" size={12}>
-          <Button onClick={getFolders}>Get folders</Button>
+          <Button onClick={getSampleFiles}>Get sample files</Button>
         </Grid>
       </Grid>
     </Box>
